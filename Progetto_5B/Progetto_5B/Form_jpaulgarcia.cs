ee9b13d03ca329bd1818c5a953023957d4362763
jpaulgarcia <jpaul_garcia_03@yahoo.com>
2016-11-21 09:47:12 +0100

    Aggiunta del form individuale

---
+++ Progetto_5B/Progetto_5B/Form_jpaulgarcia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progetto_5B
+{
+    public partial class Form_jpaulgarcia : Form
+    {
+        public Form_jpaulgarcia()
+        {
+            InitializeComponent();
+        }
+    }
+}
